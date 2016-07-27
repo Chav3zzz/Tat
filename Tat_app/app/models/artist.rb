86025f6cbbbea7ed1f75252d69a17332e6d2e3f5
Antonio Chavez <theantoniochavez@gmail.com>
2016-07-26 22:45:01 -0700

    Geocoder is working, giving out latitude and longitude of created profile.

--- Tat_app/app/models/artist.rb
+++ Tat_app/app/models/artist.rb
@@ -2,5 +2,13 @@ class Artist < ActiveRecord::Base
 
   has_many   :reviews
   belongs_to :category, optional: true
+  validates_presence_of :name
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    [address1, address2, city, state, zipcode].join(', ')
+  end
 
 end
--- Tat_app/db/schema.rb
+++ Tat_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726224638) do
+ActiveRecord::Schema.define(version: 20160727051138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160726224638) do
     t.string "phone"
     t.string "email"
     t.string "mobile"
+    t.float  "latitude"
+    t.float  "longitude"
   end
 
   create_table "categories", force: :cascade do |t|
